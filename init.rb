a841972cd0d6c510a70eacd2db2cc3fadbbc507c
Caio Amaral <caioaamaral@gmail.com>
2021-12-04 09:22:27 -0300

    Add local cmake package: 'package_that_succeeds'

--- init.rb
+++ init.rb
@@ -1,1 +1,4 @@
 require 'autoproj/git_server_configuration'
+
+Autoproj.config.set("build", File.join(Autoproj.root_dir, "build")) unless Autoproj.config.get("build", nil)
+Autoproj.config.set("source", "src") unless Autoproj.config.source_dir
